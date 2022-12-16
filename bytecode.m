dac9b769af7234fa3e5119451f92c88abaaf41e9
oneharry <harryofficialchidi@yahoo.com>
2022-12-16 15:35:42 +0000

    about time to rest

--- bytecode.m
+++ bytecode.m
@@ -1,4 +1,16 @@
-push 402
-push 98
-mod Holberton
-pall
+push 0 push 0 onto stack
+push 1 push 1 onto stack
+
+push 2
+   push 3
+                   pall
+
+
+
+push 4
+
+    push 5
+       push    6
+
+
+pall This is the end of our program. monty is awesone!
